d218aa499e8885524a2bac548ed73a2159282940
Jason Kee <jasonk2723@gmail.com>
2021-11-26 14:01:24 -0700

    displaying books work, working on creating a form to add books

---
+++ script.js
@@ -0,0 +1,47 @@
+let myLibrary = [];
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+
+    this.getInfo = function() {
+        return ('"'+ title + '"'  + ", " + author + ", " + pages + " pages");
+    }
+}
+function addBookToLibrary() {
+
+}
+
+const book1 = new Book("Harry Potter", "JK Rowling", 196);
+myLibrary.push(book1);
+
+const book2 = new Book("The Name of The Wind", "Patrick Rothfuss", 562);
+myLibrary.push(book2);
+
+const book3 = new Book("The Lightning Thief", "Rick Riordan", 265);
+myLibrary.push(book3);
+
+
+for (let i = 0; i < myLibrary.length; i++) {
+    const div = document.createElement('div');
+    div.classList.add("book");
+    div.textContent = myLibrary[i].getInfo();
+    bookList.append(div);
+}
+
+
+
+// function Book(title, author, pages) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+
+//     this.getInfo = function() {
+//         return (title + ", " + author + ", " + pages);
+//     }
+// }
+
+// const book1 = new Book("Harry Potter", "JK Rowling", 196);
+// const book2 = new Book("The Name of The Wind", "Patrick Rothfuss", 562);
+// console.log(book1.getInfo());
+// console.log(book2.getInfo());
